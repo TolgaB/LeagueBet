625553ee02ed53f8cb267f84dccd6f2705b3b6e0
Tolgab <Mathman290@gmail.com>
2016-04-02 18:31:41 -0700

    Added game pulling

--- server.js
+++ server.js
@@ -87,25 +87,28 @@ io.on('connection', function(socket){
       
     });
 
-
+    socket.on('getCurrentGames', function() {
+        socket.emit('currentGames', getGames());
+    });
         });
 
 
 
+pullUpInfoOnSpecificGame(getGames());
 function getGames() {
 client({ path: 'http://spectator.na.lol.riotgames.com:80/observer-mode/rest/featured?api_key=dd32a661-7717-4722-bcc7-31f53ca42fdb' }).then(function(response) {
-   // console.log(response.entity.gameList[0]);
+   console.log(response.entity.gameList);
     return(response.entity.gameList);  
 });
 }
 //Might not use these
 function pullUpInfoOnSpecificGame(gameId) {
-  /*
-client({ path:'https://na.api.pvp.net/api/lol/na/v2.2/match/'gameId'?api_key=dd32a661-7717-4722-bcc7-31f53ca42fdb' }).then(function(response) {
+  
+client({ path:'https://na.api.pvp.net/api/lol/na/v2.2/match/'+gameId+'?api_key=dd32a661-7717-4722-bcc7-31f53ca42fdb' }).then(function(response) {
     console.log(response.entity);
     return(response.entity);  
 });
-*/
+
 }
 //To tell if the league of legends game has ended
 function didGameEnd(gameId) {
