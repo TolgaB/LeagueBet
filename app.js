63c6aed7b07054ecd1ffd1a76f51383145216401
justinyangusa <justin@y4ng.com>
2016-04-02 22:44:02 -0700

    Sandbox working?

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 var Client = require('coinbase').Client;
 
-var client = new Client({
-  'apiKey': 'xskFxylIUOOS53RF',
-  'apiSecret': 'gQObKBi2WH7NSaU6z3b0S4TLdJCzjrQW',
-  'baseApiUri': 'https://api.sandbox.coinbase.com/v2/',
-  'tokenUri': 'https://api.sandbox.coinbase.com/oauth/token'
-});
-
-var address = null;
+var client = new Client({'apiKey': 'xskFxylIUOOS53RF',
+                         'apiSecret': 'gQObKBi2WH7NSaU6z3b0S4TLdJCzjrQW',});
 
 client.getAccount('primary', function(err, account) {
   account.sendMoney({'to': 'kevinfang28@gmail.com',
