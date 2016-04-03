e1fdceb144608f424cfcd7d251e2a0527c511ddd
justinyangusa <justin@y4ng.com>
2016-04-02 22:51:43 -0700

    Primary account default

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var Client = require('coinbase').Client;
 var client = new Client({'apiKey': 'xskFxylIUOOS53RF',
                          'apiSecret': 'gQObKBi2WH7NSaU6z3b0S4TLdJCzjrQW',});
 
-client.getAccount('primary', function(err, account) {
-  account.sendMoney({'to': 'kevinfang28@gmail.com',
+client.getAccount('primary', function(err, primary) {
+  primary.sendMoney({'to': 'kevinfang28@gmail.com',
                      'amount': '0.1',
                      'currency': 'BTC'}, function(err, tx) {
     console.log(tx);
